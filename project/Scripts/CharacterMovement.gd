2f4421d05449fd42255ab89e97f6bffa6476cfa3
Federico C <134641765+Deiuca@users.noreply.github.com>
2025-04-07 14:27:12 +0200

    Set-up player development branch
    + Test Lvl e assets
    + modified player movement script to include animations

--- project/Scripts/CharacterMovement.gd
+++ project/Scripts/CharacterMovement.gd
@@ -11,15 +11,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left_arrow", "right_arrow")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if direction != 0:
+		$AnimatedSprite2D.flip_h = direction > 0
+	
 	move_and_slide()
