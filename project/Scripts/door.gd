466a2af5328285942d9c10f54d1d36a93d0a241a
Federico C <134641765+Deiuca@users.noreply.github.com>
2025-04-13 10:33:41 +0200

    + Door funzionante
    + playerTag
    + lvl manager finito

--- project/Scripts/door.gd
+++ project/Scripts/door.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
 @export var next_scene : PackedScene = null
-@export var open := false
+var _open := false
+@export var opened_texture : Texture2D
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		player_entered()
+		_player_entered()
 
-func player_entered() -> void:
-	if open and next_scene:
+func _player_entered() -> void:
+	if _open and next_scene:
 		get_tree().change_scene_to_packed(next_scene)
+
+func open_door() -> void:
+	assert(opened_texture != null, "Manca la texture di porta aperta!")
+	$Sprite2D.texture = opened_texture
+	_open = true
--- project/Scripts/level_manager.gd
+++ project/Scripts/level_manager.gd
@@ -5,11 +5,15 @@ var num_enemies : int = 0
 var kills := 0
 var no_initial_enemies := false 
 var fliesCounterNode : Node2D = null
+var door : Area2D
 
 func _ready() -> void:
 	
-	assert(has_node("FliesCounter") != null, "NO Flies counter trovato in livello! :(")
+	assert(has_node("Door") != null, "NO Door trovata in livello! :(")
 	
+	door = find_child("Door")
+	
+	assert(has_node("FliesCounter") != null, "NO Flies counter trovato in livello! :(")
 	fliesCounterNode = find_child("FliesCounter")
 	
 	var enemies := get_tree().get_nodes_in_group("enemy")
@@ -25,7 +29,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if ! no_initial_enemies and kills >= num_enemies:
-		print("bravo")
+		door.open_door()
 
 func _on_enemy_died() -> void:
 	kills += 1
